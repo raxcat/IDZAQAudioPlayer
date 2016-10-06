375b785f5db071735fd4fee1e78499c90e80f3b6
raxcat <raxcat@gmail.com>
2016-10-06 10:53:14 +0800

    Implement seek of FlacDecoder

--- IDZAQAudioPlayer/FlacFileDecoder.mm
+++ IDZAQAudioPlayer/FlacFileDecoder.mm
@@ -131,6 +131,13 @@
     
 }
 - (BOOL)seekToTime:(NSTimeInterval)timeInterval error:(NSError*__autoreleasing*)error{
+    long seekFrame = totalFrames * sampleRate;
+    if (!FLAC__stream_decoder_seek_absolute(decoder, seekFrame)){
+        if(error!=nil){
+            *error = [NSError errorWithDomain:@"FlacFileDecoderErrorDomain" code:1050 userInfo:@{NSLocalizedDescriptionKey:@"FlacDecoder seek fail"}];
+        }
+        return NO;
+    }
     return YES;
 }
 #pragma mark - flac callbacks
